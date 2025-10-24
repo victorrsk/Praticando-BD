535b38d4855c9efbfe1bc56bd2944c545cd5c7be
João Victor <jv191207@gmail.com>
2025-10-24 18:04:38 -0300

    testando

--- Praticando/db_utils.py
+++ Praticando/db_utils.py
@@ -3,8 +3,21 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
 
-def conectar_db(conexao):
-    conn = sqlite3.connect(ROOT_PATH / conexao)
-    if conn:
-        print('Banco conectado')
-        return True
+# conecta o BD
+conn = sqlite3.connect(ROOT_PATH / 'praticando.db')
+
+def criar_cursor():
+    return conn.cursor()
+
+def inserir_registro(cursor, dados):
+    try:
+        cursor.execute('INSERT INTO usuarios (nome, email, cpf) VALUES (?, ?, ?)', dados)
+        conn.commit()
+        print('Usuário inserido com sucesso!')
+    except Exception as exc:
+        print(f'Ocorreu algum erro: {exc}')
+        conn.rollback()
+
+
+
+
--- Praticando/main.py
+++ Praticando/main.py
@@ -1,1 +1,16 @@
-from db_utils import conectar_db
+from db_utils import criar_cursor, inserir_registro
+from utils import validar_dados
+
+cursor = criar_cursor()
+
+# dados para inserir no BD
+nome = input('Seu nome: ')
+email = input('Seu email: ')
+cpf = input('Seu CPF: ')
+
+# validação dos dados com regex simples
+dados_validos = validar_dados(nome, email, cpf)
+
+if dados_validos:
+    dados_registro = (nome, email, cpf)
+    inserir_registro(cursor, dados_registro)
